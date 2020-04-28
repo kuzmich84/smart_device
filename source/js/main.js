a76c5d3dfde1505b3682ca89e02e4ff096784378
kuzmich84 <kudim@inbox.ru>
2020-04-28 14:40:38 +0300

    правит баг JS

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,6 @@ let form = popup.querySelector('.form');
 let isStorageSupport = true;
 
 // модальное окно
-
 try {
   storageName = localStorage.getItem('yourName');
   storagePhone = localStorage.getItem('phone');
@@ -22,14 +21,32 @@ try {
   isStorageSupport = false;
 }
 
+const body = document.querySelector('body');
+
 function closePopup() {
   popup.classList.remove('call-popup-show');
   overlay.classList.remove('overlay-show');
+  body.classList.remove('body-lock');
+  // if(existVerticalScroll()) {
+  //   window.scrollTo(0, body.dataset.scrollY);
+  // }
 }
+
+function existVerticalScroll() {
+  return document.body.offsetHeight > window.innerHeight
+}
+
+function getBodyScrollTop() {
+  return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop || (document.body && document.body.scrollTop));
+
+}
+
 
 
 btnHeader.addEventListener('click', function (evt) {
   evt.preventDefault();
+  body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
+
   popup.classList.add('call-popup-show');
   overlay.classList.add('overlay-show');
   if (storageName) {
@@ -46,12 +63,17 @@ btnHeader.addEventListener('click', function (evt) {
     name.focus();
   }
 
+  if(existVerticalScroll()) {
+    body.classList.add('body-lock');
+  }
+
 });
 
 
 popupBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
   closePopup();
+
 });
 
 overlay.addEventListener('click', function (evt) {
@@ -79,8 +101,8 @@ form.addEventListener('submit', function (evt) {
 });
 
 
-//Плавный скролл
-
+// Плавный скролл
+//
 // собираем все якоря; устанавливаем время анимации и количество кадров
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
   animationTime = 300,
@@ -131,3 +153,4 @@ let maskOptions = {
 };
 IMask(phone, maskOptions);
 IMask(phoneFeedback, maskOptions);
+
