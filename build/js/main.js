c51e2164bfe4dd53c02f659d0b05272b73703f1b
kuzmich84 <kudim@inbox.ru>
2020-04-28 18:11:19 +0300

    добавляется коррекция icon-logo

--- build/js/main.js
+++ build/js/main.js
@@ -1,156 +1,154 @@
-let btnHeader = document.querySelector('.btn-header');
-let popupBtn = document.querySelector('.call-popup__btn');
-let popup = document.querySelector('.call-popup');
-let overlay = document.querySelector('.overlay');
-let name = popup.querySelector('#call-name');
-let text = popup.querySelector('#call-message');
-let phone = popup.querySelector('#call-phone');
-let storageName = '';
-let storagePhone = '';
-let storageMessage = '';
-let form = popup.querySelector('.form');
-let isStorageSupport = true;
+'use strict';
 
-// модальное окно
-try {
-  storageName = localStorage.getItem('yourName');
-  storagePhone = localStorage.getItem('phone');
-  storageMessage = localStorage.getItem('message');
+(function () {
+  var btnHeader = document.querySelector('.btn-header');
+  var popupBtn = document.querySelector('.call-popup__btn');
+  var popup = document.querySelector('.call-popup');
+  var overlay = document.querySelector('.overlay');
+  var name = popup.querySelector('#call-name');
+  var text = popup.querySelector('#call-message');
+  var phone = popup.querySelector('#call-phone');
+  var storageName = '';
+  var storagePhone = '';
+  var storageMessage = '';
+  var form = popup.querySelector('.form');
+  var isStorageSupport = true;
 
-} catch (err) {
-  isStorageSupport = false;
-}
+  // модальное окно
 
-const body = document.querySelector('body');
+  try {
+    storageName = localStorage.getItem('yourName');
+    storagePhone = localStorage.getItem('phone');
+    storageMessage = localStorage.getItem('message');
 
-function closePopup() {
-  popup.classList.remove('call-popup-show');
-  overlay.classList.remove('overlay-show');
-  body.classList.remove('body-lock');
-  // if(existVerticalScroll()) {
-  //   window.scrollTo(0, body.dataset.scrollY);
-  // }
-}
-
-function existVerticalScroll() {
-  return document.body.offsetHeight > window.innerHeight
-}
-
-function getBodyScrollTop() {
-  return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop || (document.body && document.body.scrollTop));
-
-}
-
-
-
-btnHeader.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
-
-  popup.classList.add('call-popup-show');
-  overlay.classList.add('overlay-show');
-  if (storageName) {
-    name.value = storageName;
-    phone.focus();
-    if (storagePhone) {
-      phone.value = storagePhone;
-      text.focus();
-    }
-    if (storageMessage) {
-      text.value = storageMessage;
-    }
-  } else {
-    name.focus();
+  } catch (err) {
+    isStorageSupport = false;
   }
 
-  if(existVerticalScroll()) {
-    body.classList.add('body-lock');
+  var body = document.querySelector('body');
+
+  function closePopup() {
+    popup.classList.remove('call-popup-show');
+    overlay.classList.remove('overlay-show');
+    body.classList.remove('body-lock');
   }
 
-});
-
-
-popupBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  closePopup();
-
-});
-
-overlay.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  closePopup();
-});
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains('call-popup-show')) {
-      evt.preventDefault();
-      closePopup();
-    }
+  function existVerticalScroll() {
+    return document.body.offsetHeight > window.innerHeight;
   }
-});
 
-form.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  if (isStorageSupport) {
-    localStorage.setItem('yourName', name.value);
-    localStorage.setItem('phone', phone.value);
-    localStorage.setItem('message', text.value);
+  function getBodyScrollTop() {
+    return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop || (document.body && document.body.scrollTop));
   }
-  closePopup();
-});
 
+  btnHeader.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    body.dataset.scrollY = getBodyScrollTop(); // сохраним значение скролла
 
-// Плавный скролл
-//
-// собираем все якоря; устанавливаем время анимации и количество кадров
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-  animationTime = 300,
-  framesCount = 60;
-
-anchors.forEach(function (item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function (e) {
-    // убираем стандартное поведение
-    e.preventDefault();
-
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-
-    // запускаем интервал, в котором
-    let scroller = setInterval(function () {
-      // считаем на сколько скроллить за 1 такт
-      let scrollBy = coordY / framesCount;
-
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
+    popup.classList.add('call-popup-show');
+    overlay.classList.add('overlay-show');
+    if (storageName) {
+      name.value = storageName;
+      phone.focus();
+      if (storagePhone) {
+        phone.value = storagePhone;
+        text.focus();
       }
-      // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
+      if (storageMessage) {
+        text.value = storageMessage;
+      }
+    } else {
+      name.focus();
+    }
+    if (existVerticalScroll()) {
+      body.classList.add('body-lock');
+    }
+
   });
-});
 
-//аккордеон
-let footerBtn = document.querySelectorAll('.page-footer__btn-wrapper');
-for (let i = 0; i < footerBtn.length; i++) {
-  footerBtn[i].addEventListener('click', function (evt) {
-    let footerNav = this.parentElement;
-    footerNav.classList.toggle('nav-footer-active');
+
+  popupBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    closePopup();
+
   });
-}
 
-let phoneFeedback = document.querySelector('#phone');
+  overlay.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    closePopup();
+  });
 
-let maskOptions = {
-  mask: '+{7}(000)000-00-00',
-};
-IMask(phone, maskOptions);
-IMask(phoneFeedback, maskOptions);
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains('call-popup-show')) {
+        evt.preventDefault();
+        closePopup();
+      }
+    }
+  });
 
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    if (isStorageSupport) {
+      localStorage.setItem('yourName', name.value);
+      localStorage.setItem('phone', phone.value);
+      localStorage.setItem('message', text.value);
+    }
+    closePopup();
+  });
+  // Плавный скролл
+
+  // собираем все якоря; устанавливаем время анимации и количество кадров
+
+  var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+  var animationTime = 300;
+  var framesCount = 60;
+
+  anchors.forEach(function (item) {
+    // каждому якорю присваиваем обработчик события
+    item.addEventListener('click', function (e) {
+      // убираем стандартное поведение
+      e.preventDefault();
+
+      // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+      var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+      // запускаем интервал, в котором
+      var scroller = setInterval(function () {
+        // считаем на сколько скроллить за 1 такт
+        var scrollBy = coordY / framesCount;
+
+        // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+        // и дно страницы не достигнуто
+        if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+          // то скроллим на к-во пикселей, которое соответствует одному такту
+          window.scrollBy(0, scrollBy);
+        } else {
+          // иначе добираемся до элемента и выходим из интервала
+          window.scrollTo(0, coordY);
+          clearInterval(scroller);
+        }
+        // время интервала равняется частному от времени анимации и к-ва кадров
+      }, animationTime / framesCount);
+    });
+  });
+  // аккордеон
+
+  var footerBtn = document.querySelectorAll('.page-footer__btn-wrapper');
+  for (var i = 0; i < footerBtn.length; i++) {
+    footerBtn[i].addEventListener('click', function () {
+      var footerNav = this.parentElement;
+      footerNav.classList.toggle('nav-footer-active');
+    });
+  }
+
+  var phoneFeedback = document.querySelector('#phone');
+
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00',
+  };
+
+  window.IMask(phone, maskOptions);
+  window.IMask(phoneFeedback, maskOptions);
+
+})();
